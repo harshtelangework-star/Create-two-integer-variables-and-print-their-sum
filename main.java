4433ffc33aa2afa2ad7a57918fd7550c04be97e0
harshtelangework-star <harshtelangework@gmail.com>
2026-01-31 01:27:25 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+//Create two integer variables and print their sum.
+public class main{
+    public static void main(String[]args){
+
+        int a = 20;
+        int b = 20;
+
+        System.out.println(a+b);
+    }
+}
